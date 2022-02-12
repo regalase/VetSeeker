e871a56ae8eccca4c0f75fe5fe9eeb007539553a
Sean <seanernest.regala@benilde.edu.ph>
2022-02-12 18:28:19 +0800

    first commit

---
+++ ISPROJ2VetSeeker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ISPROJ2VetSeeker.MvcApplication" Language="C#" %>
---
+++ ISPROJ2VetSeeker/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ISPROJ2VetSeeker.Models
+{
+    public class UserModel
+    {
+    }
+}
