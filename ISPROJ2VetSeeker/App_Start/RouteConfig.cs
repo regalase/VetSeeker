44b512913bcd9f50527a8e02ab146be9636bba67
Sean <seanernest.regala@benilde.edu.ph>
2022-06-06 10:55:53 +0800

    updated

--- ISPROJ2VetSeeker/App_Start/RouteConfig.cs
+++ ISPROJ2VetSeeker/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ISPROJ2VetSeeker
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Accounts", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
